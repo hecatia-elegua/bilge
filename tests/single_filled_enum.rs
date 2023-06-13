40640a8d8283dfd6560c873abd383455fad7ef23
Scott Fleener <scfleener@gmail.com>
2023-06-13 11:18:10 -0400

    Use wrapping_shr in more places

--- tests/single_filled_enum.rs
+++ tests/single_filled_enum.rs
@@ -3,17 +3,17 @@ use bilge::prelude::*;
 #[bitsize(32)]
 #[derive(TryFromBits, PartialEq, DebugBits)]
 struct Wrapper {
-    foo: FillsU8
+    foo: FillsU32
 }
 
 #[bitsize(32)]
 #[derive(TryFromBits, PartialEq, Debug)]
-enum FillsU8 {
+enum FillsU32 {
     Foo = 0xDEADBEEF
 }
 
 #[test]
 fn single_filled_enum_works_issue_36() {
     let foo = Wrapper::try_from(0xDEADBEEF);
-    assert_eq!(foo, Ok(Wrapper::new(FillsU8::Foo)));
+    assert_eq!(foo, Ok(Wrapper::new(FillsU32::Foo)));
 }
