8d2f320cb6118e48b7e731124b85a021c996d0f8
pickx <pickyx@protonmail.com>
2023-06-24 19:54:38 +0300

    split example into tests

--- examples/enums_with_fallback.rs
+++ examples/enums_with_fallback.rs
@@ -5,7 +5,7 @@ use assert_matches::assert_matches;
 
 #[bitsize(7)]
 #[derive(FromBits, Debug)]
-enum HasFallback {
+enum UnitFallback {
     First,
     Second,
     Third,
@@ -15,16 +15,7 @@ enum HasFallback {
 
 #[bitsize(7)]
 #[derive(FromBits, Debug)]
-enum HasFallbackOnNonReserved {
-    First,
-    #[fallback]
-    Second,
-    Third,
-}
-
-#[bitsize(7)]
-#[derive(FromBits, Debug)]
-enum HasFallbackWithValue {
+enum FallbackWithValue {
     First,
     Second,
     Third,
@@ -32,62 +23,20 @@ enum HasFallbackWithValue {
     Reserved(u7),
 }
 
-#[bitsize(7)]
-#[derive(FromBits, Debug)]
-enum HasFallbackAndNonDefaultOrdinals {
-    First = 1,
-    Second = 3,
-    Third = 5,
-    #[fallback]
-    Reserved,
-}
-
 fn main() {
-    assert_matches!(
-        HasFallback::from(u7::new(5)), 
-        HasFallback::Reserved
-    );
-
-    assert_matches!(
-        HasFallbackOnNonReserved::from(u7::new(1)), 
-        HasFallbackOnNonReserved::Second
-    );
-    assert_matches!(
-        HasFallbackOnNonReserved::from(u7::new(9)), 
-        HasFallbackOnNonReserved::Second
-    );
-
-    assert_matches!(
-        HasFallbackWithValue::from(u7::new(0b11)), 
-        HasFallbackWithValue::Reserved(n) if n == u7::new(3)
-    );
-    assert_matches!(
-        HasFallbackWithValue::from(u7::new(0b1001)), 
-        HasFallbackWithValue::Reserved(n) if n == u7::new(9)
-    );
-
-    assert_matches!(
-        HasFallbackAndNonDefaultOrdinals::from(u7::new(0)), 
-        HasFallbackAndNonDefaultOrdinals::Reserved
-    );
-    assert_matches!(
-        HasFallbackAndNonDefaultOrdinals::from(u7::new(5)), 
-        HasFallbackAndNonDefaultOrdinals::Third
-    );
-
     // with unit fallback variants, converting back to a number will discard the fallback value,
     // if that value is not the variant's ordinal:
     let original = u7::new(7);
-    let converted = HasFallback::from(original);
-    assert_matches!(converted, HasFallback::Reserved);
+    let converted = UnitFallback::from(original);
+    assert_matches!(converted, UnitFallback::Reserved);
     assert_eq!(u7::from(converted), u7::new(3));
 
     //...but this is not true for fallback variants with a value:
     let original = u7::new(3);
-    let converted = HasFallbackWithValue::from(original);
+    let converted = FallbackWithValue::from(original);
     assert_eq!(u7::from(converted), original);
     
     let original = u7::new(9);
-    let converted = HasFallbackWithValue::from(original);
+    let converted = FallbackWithValue::from(original);
     assert_eq!(u7::from(converted), original);
 }
