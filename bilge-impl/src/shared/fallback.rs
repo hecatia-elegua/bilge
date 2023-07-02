e49d1e8096d846802501524a1c3bb78f11c06e04
pickx <pickyx@protonmail.com>
2023-07-02 20:37:01 +0300

    restrict value fallback to last variant of enum

--- bilge-impl/src/shared/fallback.rs
+++ bilge-impl/src/shared/fallback.rs
@@ -10,7 +10,7 @@ pub enum Fallback {
 }
 
 impl Fallback {
-    fn from_variant(variant: &Variant, enum_bitsize: BitSize) -> Fallback {
+    fn from_variant(variant: &Variant, enum_bitsize: BitSize, is_last_variant: bool) -> Fallback {
         use syn::Fields::*;
 
         let ident = variant.ident.to_owned();
@@ -21,10 +21,13 @@ impl Fallback {
             }
             Unnamed(fields) => {
                 let variant_fields = fields.unnamed.iter();
-
                 let Ok(fallback_value) = variant_fields.exactly_one() else {
                     abort!(variant, "fallback variant must have exactly one field"; help = "use only one field or change to a unit variant")
                 };
+                
+                if !is_last_variant {
+                    abort!(variant, "value fallback is not the last variant"; help = "a fallback variant with value must be the last variant of the enum")
+                }
 
                 match &fallback_value.ty {
                     Type::Path(type_path) => match bitsize_from_type_token(&type_path.path) {
@@ -65,7 +68,8 @@ impl Fallback {
 /// finds a single enum variant with the attribute "fallback".
 /// a "fallback variant" may come in one of two forms:
 /// 1. `#[fallback] Foo`, which we map to `Fallback::Unit`
-/// 2. `#[fallback] Foo(uN)`, where `N` is the bitsize of the enum, which we map to `Fallback::WithValue`
+/// 2. `#[fallback] Foo(uN)`, where `N` is the enum's bitsize and `Foo` is the enum's last variant, 
+/// which we map to `Fallback::WithValue`
 pub fn fallback_variant(data: &Data, enum_bitsize: BitSize) -> Option<Fallback> {
     match data {
         Data::Enum(enum_data) => {
@@ -77,7 +81,8 @@ pub fn fallback_variant(data: &Data, enum_bitsize: BitSize) -> Option<Fallback> 
             match variants_with_fallback.at_most_one() {
                 Ok(None) => None,
                 Ok(Some(variant)) => {
-                    let fallback = Fallback::from_variant(variant, enum_bitsize);
+                    let is_last_variant = variant.ident == enum_data.variants.last().unwrap().ident;
+                    let fallback = Fallback::from_variant(variant, enum_bitsize, is_last_variant);
                     Some(fallback)
                 },
                 Err(_) => abort_call_site!(
