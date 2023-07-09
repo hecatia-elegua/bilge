e350aac63c3e98b0edb652212bf8c4c991ea9a4d
hecatia-elegua <108802164+hecatia-elegua@users.noreply.github.com>
2023-07-09 17:29:09 +0200

    Add some ui tests for the attribute

---
+++ tests/ui/attr-value-is-invalid.rs
@@ -0,0 +1,23 @@
+use bilge::prelude::*;
+
+// negative value
+#[bitsize(-1)]
+enum Test {}
+
+// one below lowest value
+// #[bitsize(0)]
+// enum Test {}
+
+// one above highest (struct) value
+#[bitsize(129)]
+struct Test {}
+
+// one above highest enum value
+#[bitsize(65)]
+enum Test {}
+
+// one above highest enum value, this compiles
+#[bitsize(65)]
+struct Test { field: u65 }
+
+fn main() {}
