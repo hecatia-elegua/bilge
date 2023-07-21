e5e7719c5b5b969da2fbf6585623cbbb0cf07f92
pickx <pickyx@protonmail.com>
2023-07-21 20:51:59 +0300

    add tests

--- bilge-impl/src/shared/util.rs
+++ bilge-impl/src/shared/util.rs
@@ -1,4 +1,6 @@
 use syn::Path;
+#[cfg(test)]
+use syn_path::path;
 
 pub trait PathExt {
     /// match path segments. `str_segments` should contain the entire
@@ -39,5 +41,51 @@ impl PathExt for Path {
         let str_segments = str_segments.iter().copied().rev();
 
         segments.zip(str_segments).all(|(a, b)| a == b)
+    }
+}
+
+#[test]
+fn path_matching() {
+    let paths = [
+        path!(::std::default::Default),
+        path!(std::default::Default),
+        path!(default::Default),
+        path!(Default),
+    ];
+
+    let str_segments = &["std", "default", "Default"];
+
+    for path in paths {
+        assert!(path.matches(str_segments));
+    }
+}
+
+#[test]
+fn partial_does_not_match() {
+    let full_path = path!(std::foo::bar::fizz::Buzz);
+
+    let str_segments = ["std", "foo", "bar", "fizz", "Buzz"];
+
+    for i in 1..str_segments.len() {
+        let partial_str_segments = &str_segments[i..];
+        assert!(!full_path.matches(partial_str_segments))
+    }
+}
+
+#[test]
+fn path_matching_without_root() {
+    let paths = [
+        path!(::core::fmt::Debug),
+        path!(core::fmt::Debug),
+        path!(::std::fmt::Debug),
+        path!(std::fmt::Debug),
+        path!(fmt::Debug),
+        path!(Debug),
+    ];
+
+    let str_segments_without_root = &["fmt", "Debug"];
+
+    for path in paths {
+        assert!(path.matches_core_or_std(str_segments_without_root));
     }
 }
