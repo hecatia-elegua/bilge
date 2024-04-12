0d014aad938642a50c7bc8587ef3ddad8d530d16
widberg <jaredwidberg@gmail.com>
2024-04-12 18:29:53 -0400

    Improve serde tests

--- tests/serde.rs
+++ tests/serde.rs
@@ -6,23 +6,26 @@ use serde_test::{assert_de_tokens_error, assert_tokens, Token};
 #[bitsize(17)]
 #[derive(FromBits, PartialEq, SerializeBits, DeserializeBits, DebugBits)]
 struct BitsStruct {
-    field1: u8,
-    field2: u8,
     padding: u1,
+    reserved: u1,
+    field1: u8,
+    padding: u1,
+    field2: u5,
+    reserved: u1,
 }
 
 #[test]
 fn serde_struct() {
-    let bits = BitsStruct::from(u17::new(0b0_0000_0001_0010_0011));
+    let bits = BitsStruct::from(u17::new(0b0_01001_0_00100011_0_0));
 
     assert_tokens(
         &bits,
         &[
             Token::Struct { name: "BitsStruct", len: 2 },
             Token::Str("field1"),
-            Token::U8(0b0010_0011),
+            Token::U8(0b00100011),
             Token::Str("field2"),
-            Token::U8(0b0000_0001),
+            Token::U8(0b01001),
             Token::StructEnd,
         ],
     );
@@ -34,7 +37,7 @@ fn serde_struct_missing_field() {
         &[
             Token::Struct { name: "BitsStruct", len: 1 },
             Token::Str("field1"),
-            Token::U8(0b0010_0011),
+            Token::U8(0b00100011),
             Token::StructEnd,
         ],
         "missing field `field2`",
@@ -47,22 +50,22 @@ fn serde_struct_extra_field() {
         &[
             Token::Struct { name: "BitsStruct", len: 3 },
             Token::Str("field1"),
-            Token::U8(0b0010_0011),
+            Token::U8(0b00100011),
             Token::Str("field2"),
-            Token::U8(0b0000_0001),
+            Token::U8(0b01001),
             Token::Str("field3"),
         ],
         "unknown field `field3`, expected `field1` or `field2`",
     );
 }
 
-#[bitsize(16)]
+#[bitsize(13)]
 #[derive(FromBits, PartialEq, SerializeBits, DeserializeBits, DebugBits)]
-struct BitsTupleStruct(u8, u8);
+struct BitsTupleStruct(u8, u5);
 
 #[test]
 fn serde_tuple_struct() {
-    let bits = BitsTupleStruct::from(0b0000_0001_0010_0011);
+    let bits = BitsTupleStruct::from(u13::new(0b01001_00100011));
 
     assert_tokens(
         &bits,
@@ -71,8 +74,8 @@ fn serde_tuple_struct() {
                 name: "BitsTupleStruct",
                 len: 2,
             },
-            Token::U8(0b0010_0011),
-            Token::U8(0b0000_0001),
+            Token::U8(0b00100011),
+            Token::U8(0b01001),
             Token::TupleStructEnd,
         ],
     );
