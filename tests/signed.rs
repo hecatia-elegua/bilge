83f09cb1d4fcd2c28612a034666d030116e5c067
widberg <widberg@widberg.me>
2025-09-06 09:58:34 -0400

    Add more tests

--- tests/signed.rs
+++ tests/signed.rs
@@ -23,3 +23,66 @@ fn bit_struct_signed() {
     let y = i7::new(42);
     assert_eq!(bits.y(), y);
 }
+
+#[bitsize(32)]
+#[derive(DebugBits, FromBits)]
+struct TupleStructSigned(u2, i6, i7, u8, u8, i1);
+
+#[test]
+fn tuple_struct_signed() {
+    let val_0 = u2::new(0);
+    let val_1 = i6::new(-30);
+    let val_2 = i7::new(-9);
+    let val_3 = u8::new(3);
+    let val_4 = u8::new(4);
+    let val_5 = i1::new(-1);
+    let mut a = TupleStructSigned::new(val_0, val_1, val_2, val_3, val_4, val_5);
+    assert_eq!(a.val_0(), val_0);
+    assert_eq!(a.val_1(), val_1);
+    assert_eq!(a.val_2(), val_2);
+    assert_eq!(a.val_3(), val_3);
+    assert_eq!(a.val_4(), val_4);
+    assert_eq!(a.val_5(), val_5);
+    let val_1 = i6::new(25);
+    a.set_val_1(val_1);
+    assert_eq!(a.val_0(), val_0);
+    assert_eq!(a.val_1(), val_1);
+    assert_eq!(a.val_2(), val_2);
+    assert_eq!(a.val_3(), val_3);
+    assert_eq!(a.val_4(), val_4);
+    assert_eq!(a.val_5(), val_5);
+    let val_2 = i7::MIN;
+    a.set_val_2(val_2);
+    assert_eq!(a.val_0(), val_0);
+    assert_eq!(a.val_1(), val_1);
+    assert_eq!(a.val_2(), val_2);
+    assert_eq!(a.val_3(), val_3);
+    assert_eq!(a.val_4(), val_4);
+    assert_eq!(a.val_5(), val_5);
+}
+
+#[bitsize(16)]
+#[derive(FromBits, DebugBits)]
+struct EthercatHeader1Signed {
+    len: i11,
+    some: i1,
+    ty: u4,
+}
+
+#[bitsize(16)]
+#[derive(FromBits, DebugBits)]
+struct EthercatHeader2Signed {
+    len: i11,
+    reserved: i1,
+    ty: u4,
+}
+
+#[test]
+fn should_be_same_structure_issue_30_signed() {
+    let ty = u4::new(0x1);
+    let eh1 = EthercatHeader1Signed::new(i11::new(0xe), i1::new(0), ty);
+    let eh2 = EthercatHeader2Signed::new(i11::new(0xe), ty);
+    assert_eq!(eh1.ty(), ty);
+    assert_eq!(eh2.ty(), ty);
+    assert_eq!(eh1.value, eh2.value);
+}
